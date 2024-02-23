692b76001a162b54863d6df13c2cc6933e865770
Yeferson Mejias <yefer.el.teke@gmail.com>
2024-02-23 10:21:39 -0500

    chore: optmize build

--- packages/core/src/queuellyItem.ts
+++ packages/core/src/queuellyItem.ts
@@ -1,5 +1,3 @@
-import shortid from "shortid"
-
 enum QueuellyState {
   None,
   Pending,
@@ -8,13 +6,13 @@ enum QueuellyState {
 }
 
 export interface QueuellyOptions<V> {
-  name: string;
-  action: () => Promise<V>;
-  depends?: string[];
-  waitFor?: string[];
-  canReplace?: boolean;
-  onComplete?(value: V, ctx: { isLast: boolean }): void;
-  onError?(reason: any, ctx: { isLast: boolean, lastValue: V | undefined }): void;
+  name: string
+  action: () => Promise<V>
+  depends?: string[]
+  waitFor?: string[]
+  canReplace?: boolean
+  onComplete?(value: V, ctx: { isLast: boolean }): void
+  onError?(reason: any, ctx: { isLast: boolean; lastValue: V | undefined }): void
 }
 
 export interface QueuellyItem<V> extends Omit<QueuellyOptions<V>, "depends" | "waitFor"> {
@@ -39,17 +37,17 @@ export function createQueuellyItem<V>(options: QueuellyOptions<V>): QueuellyItem
   return {
     ...options,
     // TODO: Optimize or delete this
-    id: shortid(),
+    id: Math.random().toString(36).substring(2, 7),
     depends: (name) => !!options.depends?.includes(name),
     waitFor: (name) => !!options.waitFor?.includes(name),
-    pending: () => state = QueuellyState.Pending,
+    pending: () => (state = QueuellyState.Pending),
     isPending: () => state === QueuellyState.Pending,
-    complete: () => state = QueuellyState.Complete,
+    complete: () => (state = QueuellyState.Complete),
     isComplete: () => state === QueuellyState.Complete,
     isFinally: () => state === QueuellyState.Complete || state === QueuellyState.Error,
-    error: () => state = QueuellyState.Error,
+    error: () => (state = QueuellyState.Error),
     isError: () => state === QueuellyState.Error,
     isInitialized: () => state !== QueuellyState.None,
-    promise: () => options.action()
+    promise: () => options.action(),
   }
 }
